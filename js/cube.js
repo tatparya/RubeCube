adff37a8a18808474766312c0eb4f4623a24b7b2
Tatparya Shankar <tshankar@purdue.edu>
2016-12-22 00:13:53 +0530

    Init commit, added materialize

---
+++ js/cube.js
@@ -0,0 +1,39 @@
+//  Init cube
+var side1 = [[0,0,0],[0,0,0],[0,0,0]];
+var side2 = [[1,1,1],[1,1,1],[1,1,1]];
+var side3 = [[2,2,2],[2,2,2],[2,2,2]];
+var side4 = [[3,3,3],[3,3,3],[3,3,3]];
+var side5 = [[4,4,4],[4,4,4],[4,4,4]];
+var side6 = [[5,5,5],[5,5,5],[5,5,5]];
+
+var cube = [ side1, side2, side3, side4, side5, side6 ];
+
+var rotateFace = function( side ){
+    for( i = 0; i < 2; i++ )
+    {
+        temp = side[0][i];
+        side[0][i] = side[2-i][0];
+        side[2-i][0] = side[2][2-i];
+        side[2][2-i] = side[i][2];
+        side[i][2] = temp;
+    }
+}
+
+var moveU = function( cube, side ){
+    switch( side ){
+        case 1:
+            temp = side1[0];
+            side1[0] = side2[0];
+            side2[0] = side3[0];
+            side3[0] = side4[0];
+            side4[0] = temp;
+            rotateFace( side5 );
+            break;
+        case 2: break;
+        case 3: break;
+        case 4: break;
+        case 5: break;
+        case 6: break;
+
+    }
+}
