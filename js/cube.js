5317654b697c2292661a66fe1a2e627fe19ac715
Tatparya Shankar <tshankar@purdue.edu>
2016-12-23 00:45:33 +0530

    Cube representation complete

--- js/cube.js
+++ js/cube.js
@@ -1,4 +1,5 @@
 //  Init cube
+var invSide = [[-1,-1,-1],[-1,-1,-1],[-1,-1,-1]];
 var side1 = [[0,0,0],[0,0,0],[0,0,0]];
 var side2 = [[1,1,1],[1,1,1],[1,1,1]];
 var side3 = [[2,2,2],[2,2,2],[2,2,2]];
@@ -19,11 +20,64 @@ var rotateFace = function( side ){
     }
 }
 
+var setColor = function( obj, color ){
+    switch( color ){
+        case 0: obj.style.backgroundColor = "#d50000"; // red;
+                break;
+        case 1: obj.style.backgroundColor = "#2962ff"; // blue;
+                break;
+        case 2: obj.style.backgroundColor = "#00c853"; // green;
+                break;
+        case 3: obj.style.backgroundColor = "#fafafa"; // grey;
+                break;
+        case 4: obj.style.backgroundColor = "#ff6d00"; // orange;
+                break;
+        case 5: obj.style.backgroundColor = "#ffd600"; // yellow;
+                break;
+        default:
+                break;
+    }
+}
+
 var printSide = function( side, sideObj ){
-    var sideDiv = document.createElement( "div" );
+    //  Create three rows
+    console.log( "Printing cube" );
+    for( i = 0; i < 3; i++ ){
+        var cubeRow = document.createElement( "div" );
+        cubeRow.className = "cube-row";
+        //  Create three blocks
+        for( j = 0; j < 3; j++ ){
+            var cubeBlock = document.createElement( "div" );
+            cubeBlock.className = "cube-block";
+            //  Set block color
+            setColor( cubeBlock, side[i][j] );
+            //  Append block
+            cubeRow.appendChild( cubeBlock );
+        }
+        //  Append row
+        sideObj.appendChild( cubeRow );
+    }
+}
+
+var initCube = function(){
+    //  Populate invalid sides
+    var invSides = document.getElementsByClassName( "invSide" );
+    for( itr = 0; itr < invSides.length; itr++ ){
+        console.log( itr );
+        printSide( invSide, invSides[itr] );
+    }
+
+    //  Populate sides
+    for( itr = 0; itr < 6; itr++ ){
+        //  Get cube side html object
+        console.log( "side" + (itr + 1) );
+        var sideObj = document.getElementById( "side" + (itr + 1) );
+        printSide( cube[itr], sideObj );
+    }
 }
 
 console.log( cube );
+initCube();
 
 var moveU = function( cube, side ){
 
