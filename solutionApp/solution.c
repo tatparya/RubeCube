141e8b427a88d8adadd9f793cec61754e8f5c672
Tatparya Shankar <tshankar@purdue.edu>
2017-02-15 02:24:48 -0500

    Solved up till second layer [Layer2][Done][Working]

--- solutionApp/solution.c
+++ solutionApp/solution.c
@@ -44,13 +44,18 @@ void solveCube( int cube[6][3][3] )
     };
     // displayCorners( corners );
 
-    printf( "Solving White Cross\n" );
+    printf( "~~~~~~~~~  Solving White Cross ~~~~~~~~~\n" );
     solveWhiteCross( cube, edges );
 
     displayCube( cube );
 
-    printf( "Solving White Corners\n" );
+    printf( "~~~~~~~~~ Solving White Corners ~~~~~~~~~\n" );
     sovleWhiteCorners( cube, corners );
+
+    displayCube( cube );
+
+    printf( "~~~~~~~~~ Solving Second Layer ~~~~~~~~~\n" );
+    solveSecondLayer( cube, edges );
 
     displayCube( cube );
 
@@ -353,9 +358,217 @@ void sovleWhiteCorners( int cube [6][3][3], int * corners [2][4][3] )
     }
 }
 
-void solveSecondLayer( int cube [6][3][3] )
+void solveSecondLayer( int cube [6][3][3], int * edges [3][4][2] )
 {
+    int count = 0;
+    int other = 0;
+    int dst = 0;
+    int layer = 0;
+    int move = 0;
+    int cornerCount = 0;
+    int cur = 0;
 
+
+    while( count < 4 )
+    {
+        // displayCube( cube );
+        count = 0;
+        //  Check if done
+        for( int side = 0; side < 4; side++ )
+        {
+            //  Check if edge in place
+            if( *edges[1][side][0] == side )
+            {
+                other = *edges[1][side][1];
+                dst = side - 1;
+                dst = (dst == -1) ? 3 : dst;
+
+                if( other == dst )
+                {
+                    count++;
+                }
+            }
+        }
+
+        //  Check if doen
+        if( count == 4 )
+        {
+            continue;
+        }
+
+        //  Layer 2
+        layer = 2;
+        int side = 0;
+        while( side < 4 )
+        {
+            //  Check if not orange
+            if( *edges[layer][side][1] != 5 && *edges[layer][side][0] != 5 )
+            {
+                printf( "~~~~~~~~~~~~~~~~~~\t");
+                printf( "Not orange found at layer 2 side %d", side );
+                printf( "\t~~~~~~~~~~~~~~~~~~\n");
+                printf( "Colors found : %d, %d\n", *edges[layer][side][1], *edges[layer][side][0] );
+                //  Bring above spot
+                cur = *edges[layer][side][0];
+                dst = *edges[layer][side][1];
+
+                //  Get into right spot
+                move = ( dst - side ) % 4;
+                move = ( move == -3 ) ? 1 : move;
+                move = ( move == 3 ) ? -1 : move;
+
+                printf( "M: %d, D: %d\n", move, dst );
+
+                //  Get piece below right spot
+                if( move == -1 )
+                {
+                    calcMove( cube, 'D', side );
+                }
+                else if( move == 1 )
+                {
+                    calcMove( cube, 'd', side );
+                }
+                else if( move == -2 || move == 2 )
+                {
+                    calcMove( cube, 'd', side );
+                    calcMove( cube, 'd', side );
+                }
+
+                //  SPECIAL CASE
+                if( ( cur == 3 && dst == 0 ) )
+                {
+                    printf( "~~~~~~~~~~~~~~~~~~\t");
+                    printf( "Moving to left" );
+                    printf( "\t~~~~~~~~~~~~~~~~~~\n");
+                    calcMove( cube, 'd', dst );
+                    calcMove( cube, 'l', dst );
+                    calcMove( cube, 'D', dst );
+                    calcMove( cube, 'L', dst );
+                    calcMove( cube, 'D', dst );
+                    calcMove( cube, 'F', dst );
+                    calcMove( cube, 'd', dst );
+                    calcMove( cube, 'f', dst );
+                    side = 0;
+                    continue;
+                }
+                //  SPECIAL CASE
+                if( ( dst == 3 && cur == 0 ) )
+                {
+                    printf( "~~~~~~~~~~~~~~~~~~\t");
+                    printf( "Moving to right" );
+                    printf( "\t~~~~~~~~~~~~~~~~~~\n");
+                    calcMove( cube, 'D', dst );
+                    calcMove( cube, 'R', dst );
+                    calcMove( cube, 'd', dst );
+                    calcMove( cube, 'r', dst );
+                    calcMove( cube, 'd', dst );
+                    calcMove( cube, 'f', dst );
+                    calcMove( cube, 'D', dst );
+                    calcMove( cube, 'F', dst );
+                    side = 0;
+                    continue;
+                }
+
+                //  LEFT
+                if( cur < dst )
+                {
+                    printf( "~~~~~~~~~~~~~~~~~~\t");
+                    printf( "Moving to left" );
+                    printf( "\t~~~~~~~~~~~~~~~~~~\n");
+                    calcMove( cube, 'd', dst );
+                    calcMove( cube, 'l', dst );
+                    calcMove( cube, 'D', dst );
+                    calcMove( cube, 'L', dst );
+                    calcMove( cube, 'D', dst );
+                    calcMove( cube, 'F', dst );
+                    calcMove( cube, 'd', dst );
+                    calcMove( cube, 'f', dst );
+                    side = 0;
+                    continue;
+                }
+                //  RIGHT
+                else
+                {
+                    printf( "~~~~~~~~~~~~~~~~~~\t");
+                    printf( "Moving to right" );
+                    printf( "\t~~~~~~~~~~~~~~~~~~\n");
+                    calcMove( cube, 'D', dst );
+                    calcMove( cube, 'R', dst );
+                    calcMove( cube, 'd', dst );
+                    calcMove( cube, 'r', dst );
+                    calcMove( cube, 'd', dst );
+                    calcMove( cube, 'f', dst );
+                    calcMove( cube, 'D', dst );
+                    calcMove( cube, 'F', dst );
+                    side = 0;
+                    continue;
+                }
+            }
+
+            side++;
+        }
+
+        //  Layer 0
+        layer = 1;
+        for( int side = 0; side < 4; side++ )
+        {
+            //  Check if edge is in right place
+            if( *edges[1][side][0] == side )
+            {
+                other = *edges[1][side][1];
+                dst = side - 1;
+                dst = (dst == -1) ? 3 : dst;
+
+                if( other == dst )
+                {
+                    printf( "Edge on side %d in right place!\n", side );
+                    continue;
+                }
+            }
+
+            //  Check if edge is flipped
+            if( *edges[1][side][1] == side )
+            {
+                other = *edges[1][side][0];
+                dst = side - 1;
+                dst = (dst == -1) ? 3 : dst;
+
+                if( other == dst )
+                {
+                    printf( "~~~~~~~~~~~~~~~~~~\t");
+                    printf( "Edge on side %d is flipped!", side );
+                    printf( "\t~~~~~~~~~~~~~~~~~~\n");
+                    //  Move edge to layer 2
+                    calcMove( cube, 'd', side );
+                    calcMove( cube, 'l', side );
+                    calcMove( cube, 'D', side );
+                    calcMove( cube, 'L', side );
+                    calcMove( cube, 'D', side );
+                    calcMove( cube, 'F', side );
+                    calcMove( cube, 'd', side );
+                    calcMove( cube, 'f', side );
+                }
+            }
+
+            //  Find not orange
+            if( *edges[layer][side][1] != 5 && *edges[layer][side][0] != 5 )
+            {
+                printf( "~~~~~~~~~~~~~~~~~~\t");
+                printf( "Edge on side %d needs to go to layer 2!", side );
+                printf( "\t~~~~~~~~~~~~~~~~~~\n");
+                //  Move not orange out to layer 2
+                calcMove( cube, 'd', side );
+                calcMove( cube, 'l', side );
+                calcMove( cube, 'D', side );
+                calcMove( cube, 'L', side );
+                calcMove( cube, 'D', side );
+                calcMove( cube, 'F', side );
+                calcMove( cube, 'd', side );
+                calcMove( cube, 'f', side );
+            }
+        }
+
+    }
 }
 
 void solveOrangeCross( int cube [6][3][3] )
